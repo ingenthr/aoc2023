31896a6dc3ecc53aaa12e3bd9ceda34053bf14eb
Matt Ingenthron <ingenthr@cep.net>
2023-11-30 23:17:43 -0800

    Day 1 working.

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -3,24 +3,43 @@ use std::io::{self, BufReader};
 use std::fs::File;
 
 
+// TODO: return the value to main, print there
+// TODO: read front and back in parallel?
+
 
 fn main() -> std::io::Result<()> {
-    println!("Hello, world!");
-
-    read_in()
+    sum_calvals()
 }
 
-fn read_in() -> std::io::Result<()> {
+fn sum_calvals() -> std::io::Result<()> {
     let f = File::open("input.txt")?;
-    let mut reader = BufReader::new(f);
+    let reader = BufReader::new(f);
 
-    let mut line = String::new();
-    reader.read_line(&mut line)?;
     let lines = reader.lines().collect::<io::Result<Vec<String>>>()?;
 
+    let mut values: Vec<u64> = Vec::new();
+    let mut res_string: String;
+
     for line in lines {
-        println!("{}", line);
+        res_string = String::new();
+        let mut nums: Vec<char> = Vec::new();
+        for char in line.chars() {
+            // take the first and last number
+            if char.is_numeric() {
+                nums.push(char);
+            }
+        }
+        res_string.push(nums[0]);
+        res_string.push(nums[nums.len()-1]);
+        values.push(res_string.parse::<u64>().unwrap());
     }
+
+    let mut sum = 0;
+    for val in values {
+        sum = sum+val;
+    }
+
+    println!("{}", sum);
 
     Ok(())
 
