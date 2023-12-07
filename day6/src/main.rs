62e118e3442393fd55881c96cfeed4bc787cf0ab
Matt Ingenthron <ingenthr@cep.net>
2023-12-06 21:08:32 -0800

    Idiomatic file handling, logic still WIP.

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use std::fs;
+
+fn main() -> Result<(), io::Error> {
+    let rrecords = fs::read_to_string("input.txt")?;
+
+    let time_line = rrecords.lines().next().expect("Should have a line of times.");
+    let dist_line = rrecords.lines().next().expect("Should have a line of distances.");
+
+    let times = time_line.split_terminator(":")
+        .nth(1)
+        .expect("Expected times after a `:`.")
+        .split_whitespace()
+        .collect::<Vec<&str>>();
+    let dists = dist_line.split_terminator(":")
+        .nth(1)
+        .expect("Expected distances after a `:`.")
+        .split_whitespace()
+        .collect::<Vec<&str>>();
+    println!("{}", time_line);
+
+    for (i, dist) in dists.iter().enumerate() {
+        println!("Column {} is {} using time {}", i, dist, times[i]); 
+    }
+
+    Ok(())
+
 }
